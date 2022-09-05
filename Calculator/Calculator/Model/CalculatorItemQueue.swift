a28e5ddc3247c7f96811318657dcd6a570f13ece
wormsJJG <jjr66461837@gmail.com>
2022-09-05 10:18:53 +0900

    feat: queue구현

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,29 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by 정재근 on 2022/09/05.
+//
+
+import Foundation
+
+struct Queue<T> {
+    private var queue: [T] = []
+    
+    public var count: Int {
+        return queue.count
+    }
+    
+    public var isEmpty: Bool {
+        return queue.isEmpty
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        queue.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        return isEmpty ? nil : queue.removeFirst()
+    }
+}
+    
