773b4fb9d81b6b9302e1b417e05dce14051fa7d2
wormsJJG <jjr66461837@gmail.com>
2022-09-06 11:02:32 +0900

    Fix: Queue 파일 생성

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -7,23 +7,5 @@
 
 import Foundation
 
-struct Queue<T> {
-    private var queue: [T] = []
-    
-    public var count: Int {
-        return queue.count
-    }
-    
-    public var isEmpty: Bool {
-        return queue.isEmpty
-    }
-    
-    public mutating func enqueue(_ element: T) {
-        queue.append(element)
-    }
-    
-    public mutating func dequeue() -> T? {
-        return isEmpty ? nil : queue.removeFirst()
-    }
-}
+
     
---
+++ Calculator/Calculator/Model/Queue.swift
@@ -0,0 +1,28 @@
+//
+//  Queue.swift
+//  Calculator
+//
+//  Created by 정재근 on 2022/09/06.
+//
+
+import Foundation
+
+struct Queue<T> {
+    private var queue: [T] = []
+    
+    public var count: Int {
+        return queue.count
+    }
+    
+    public var isEmpty: Bool {
+        return queue.isEmpty
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        queue.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        return isEmpty ? nil : queue.removeFirst()
+    }
+}
