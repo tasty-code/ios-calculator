7817e6022f88b41a9a503c3468f4f8c0c2add60a
lcy <pgbenny0035@gmail.com>
2022-09-07 11:08:07 +0900

    feat: Queue타입 기능 추가

--- Calculator/Calculator/Queue.swift
+++ Calculator/Calculator/Queue.swift
@@ -1,17 +1,25 @@
 import Foundation
 
-class Queue<T> {
-    public var list: [T] = []
+class Queue<Element> {
+    public var list: [Element] = []
     
     public func removeAll() {
         list = []
     }
     
-    public func enqueue(_ item: T) {
+    public func enqueue(_ item: Element) {
         list.append(item)
     }
     
     public func dequeue() {
         list.removeLast()
+    }
+    
+    public func isEmpty() -> Bool {
+        return list.isEmpty
+    }
+    
+    public func getListCount() -> Int {
+        return list.count
     }
 }
