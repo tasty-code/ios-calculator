40361657ca31f7aaf5c922ef81584f1c02fb3315
wormsJJG <jjr66461837@gmail.com>
2022-09-14 15:34:37 +0900

    Feat: Parse 함수 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,16 +9,43 @@ import Foundation
 
 enum ExpressionParser {
     func parse(from input: String) -> Formula {
+        let formula = componentsByOperator(from: input)
+        var operands = CalculatorItemQueue<Double>()
+        var operators = CalculatorItemQueue<Character>()
         
+        for i in 0..<formula.count {
+            if i == 0 || i % 2 == 0 {
+                operands.enqueue(Double(formula[i]) ?? 0)
+            } else {
+                operators.enqueue(Operator(rawValue: Character(formula[i]))?.rawValue ?? "n")
+            }
+        }
+        return Formula(operands: operands, operators: operators)
     }
     
     private func componentsByOperator(from input: String) -> [String] {
-        var operators: [String] = []
-        for value in input {
-            if !value.isNumber {
-                operators.append(String(value))
+            let operands: [String] = input.components(separatedBy: ["+", "-", "/", "*"])
+            var operators: [String] = []
+            var result: [String] = []
+            var j = 0
+            var h = 0
+        
+            for val in input {
+                if !val.isNumber {
+                    operators.append(String(val))
+                }
             }
-        }
-        return operators
+
+            for i in 0..<operands.count+operators.count {
+                if i == 0 || i % 2 == 0 {
+                    result.append(operands[h])
+                    h += 1
+                } else {
+                    result.append(operators[j])
+                    j += 1
+                }
+            }
+        
+            return result
     }
 }
