6f4083f066aea4dc4ad7e0e459f8d2511824bcfe
wormsJJG <jjr66461837@gmail.com>
2022-09-14 10:17:52 +0900

    Feat: ExpressionParser 생성

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,18 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by 정재근 on 2022/09/14.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    func parse(from input: String) -> Formula {
+        return Formula
+    }
+    
+    func componentsByOperator(from input: Character) -> [String] {
+        return [String]
+    }
+}
