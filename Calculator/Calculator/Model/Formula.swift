5bd7f75240f242f8dead1f5f1e155d792601ad62
lcy <pgbenny0035@gmail.com>
2022-09-14 14:40:08 +0900

    refector: Formula result메서드 mutating 키워드 추가

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -4,7 +4,7 @@ struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<Character>
     
-    func result() -> Double {
+    mutating func result() -> Double {
         guard let lhs = operands.dequeue(), let rhs = operands.dequeue(), let _operator = operators.dequeue(),
               let result = Operator(rawValue: _operator)?.calculate(lhs: lhs, rhs: rhs) else {
             return 0
