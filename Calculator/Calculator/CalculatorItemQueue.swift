e41ddcf6bb8166d883145f44346241cccefaffa8
CODEMASTERJO <107622687+CODEMASTERJO@users.noreply.github.com>
2023-01-16 17:05:17 +0900

    feat. 단방향 연결 리스트 구성

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -7,3 +7,18 @@
 
 import Foundation
 
+class CalculatorItemQueue {
+    private var head: Node
+
+    
+    func append(number: Int, isPostive: Bool) {
+        if head == nil {
+            head = Node
+            return
+        }
+        var node = head
+        while node?.next != nil {
+            node = node?.next
+        }
+        node?.next = Node(number: number, operater: oprater, isPostive: isPostive)
+}
