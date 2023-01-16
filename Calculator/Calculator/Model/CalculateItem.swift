295e25df8e29a33554f1e0311f77fa843896ef84
ueunli <cdenen20@gmail.com>
2023-01-16 10:41:56 +0900

    [feat] MVC패턴 적용
    - 구조체(CalculatorItemQueue)와 프로토콜(CalculateItem) 구현
    - Model, View, Controller 분리

---
+++ Calculator/Calculator/Model/CalculateItem.swift
@@ -0,0 +1,12 @@
+//
+//  CalculateItem.swift
+//  Calculator
+//
+//  Created by J.E on 2023/01/16.
+//
+
+import Foundation
+
+protocol CalculateItem {
+    
+}
---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by J.E on 2023/01/16.
+//
+
+import Foundation
+
+struct CalculatorItemQueue: CalculateItem {
+    
+}
