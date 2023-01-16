32ec596e14c06b0fd4d5c0b11f796976601d1588
Jason <ggj00125@gmail.com>
2023-01-16 16:48:29 +0900

    Feat: UnitTest 진행을 위해 프로젝트에 CalculatorTests 파일 추가

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Jason on 2023/01/16.
+//
+
+import XCTest
+
+final class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+}
