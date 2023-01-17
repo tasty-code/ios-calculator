3403d38200af0ff1f089095a777618302dc4e6b7
qwerty3345 <qwerty3345@naver.com>
2023-01-17 13:15:07 +0900

    fix: CalculateItem 프로토콜을 Queue의 요소의 제약사항으로 변경

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct CalculatorItemQueue<Element>: CalculateItem {
+struct CalculatorItemQueue<Element: CalculateItem> {
     var elements = LinkedList<Element>()
     
     var head: Element? {
