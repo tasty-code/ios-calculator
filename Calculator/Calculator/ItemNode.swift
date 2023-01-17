3cb1bc0d3a33fac7fdbff7193b3b99b6a846a1ba
SunnnySong <tjsghktjswls@naver.com>
2023-01-17 17:02:43 +0900

    feat: ItemNode 생성

---
+++ Calculator/Calculator/ItemNode.swift
@@ -0,0 +1,24 @@
+//
+//  CalculatorItemNode.swift
+//  Calculator
+//
+//  Created by 송선진 on 2023/01/17.
+//
+
+import Foundation
+
+class ItemNode {
+    var data: Value
+    var next: ItemNode?
+    
+    init(data: Value, next: ItemNode? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
+
+
+struct Value {
+    var operators: Operator?
+    var number: Double
+}
--- Calculator/Calculator/Operator.swift
+++ Calculator/Calculator/Operator.swift
@@ -13,3 +13,5 @@ enum Operator {
     case divide
     case multiply
 }
+
+
