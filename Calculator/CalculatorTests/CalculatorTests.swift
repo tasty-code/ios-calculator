05e3ef5445808d60343ce8441fe9c0496d3cbca8
Jason <ggj00125@gmail.com>
2023-01-17 10:48:54 +0900

    Feat: inqueue 메서드 실패 테스트 코드 구현

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -13,11 +13,22 @@ final class CalculatorTests: XCTestCase {
     var sut: CalculatorItemQueue!
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut = CalculatorIemQueue()
+        sut = CalculatorItemQueue()
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         sut = nil
+    }
+    
+    func test_원하는_데이터가_들어오는가() {
+        // given
+        let wishData = 0.0
+        
+        // when
+        let result = sut.inqueue(data: wishData)
+        
+        // then
+        XCTAssertEqual(wishData, false)
     }
 }
