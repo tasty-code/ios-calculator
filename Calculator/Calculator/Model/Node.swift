9333b8ac77d39461b1f9feacf84fc77ff6a7e282
Jason <ggj00125@gmail.com>
2023-01-17 10:47:48 +0900

    Refactor: UnitTest 진행을 위해 operators 프로퍼티 옵셔널 처리

--- Calculator/Calculator/Model/Node.swift
+++ Calculator/Calculator/Model/Node.swift
@@ -10,9 +10,9 @@ import Foundation
 final class  Node {
     var next: Node?
     var data: Double
-    var operators: Operators
+    var operators: Operators?
     
-    init(next: Node? = nil, data: Double, operators: Operators) {
+    init(next: Node? = nil, data: Double, operators: Operators?) {
         self.next = next
         self.data = data
         self.operators = operators
