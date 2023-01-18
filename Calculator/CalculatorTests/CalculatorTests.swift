1806d966a0b7f87264bf3c4817a04c59fa72261c
sloDevz <onec555@gmail.com>
2023-01-18 13:50:21 +0900

    Test: StringExtension의 split이 원하는대로 나눠지는지 실패

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -54,4 +54,28 @@ final class CalculatorTests: XCTestCase {
         //than
         XCTAssertNotNil(input.next)
     }
+    
+    func test_StringExtension의_split에_String이_String배열로_나오는지() {
+        // given
+        var input = "StringTest"
+        
+        // when
+        let expectation = ["StringTest"]
+        
+        // then
+        let result = input.split(with: "a")
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_StringExtension의_split에_String이_with에_입력된_파라미터값_기준으로_나눠지는지() {
+        // given
+        var input = "String+Test"
+        
+        // when
+        let expectation = ["String","Test"]
+        
+        // then
+        let result = input.split(with: "+")
+        XCTAssertEqual(result, expectation)
+    }
 }
