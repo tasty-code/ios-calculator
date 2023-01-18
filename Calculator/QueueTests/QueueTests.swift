991717a7217bfb6daf344c1e76ce59227adddd37
ohdair <ohdiar@gmail.com>
2023-01-18 09:59:16 +0900

    refactor: 변경된 CalculateItem에 맞게끔 테스트 변경

--- Calculator/QueueTests/QueueTests.swift
+++ Calculator/QueueTests/QueueTests.swift
@@ -8,32 +8,26 @@
 import XCTest
 
 final class QueueTests: XCTestCase {
-    var sut: CalculateItemQueue<Item>!
+    var sut: CalculateItemQueue<Operator>!
     
     override func setUpWithError() throws {
-        sut = CalculateItemQueue<Item>()
+        sut = CalculateItemQueue<Operator>()
     }
 
     override func tearDownWithError() throws {
         sut = nil
     }
 
-    func test_enqueue로_item이_들어갔는지_확인() {
-        let input: Item = Item(value: 13.0, operator: .addition)
+    func test_enqueue로_Operator가_들어갔는지_확인() {
+        let input: Operator = .addition
         sut.enqueue(input)
         XCTAssertEqual(sut.queue, [input])
     }
 
-    func test_dequeue의_반환값이_item인지_확인() {
-        let input: Item = Item(value: 13.0, operator: .addition)
+    func test_dequeue의_반환값이_Operator인지_확인() {
+        let input: Operator = .division
         sut.enqueue(input)
         XCTAssertEqual(sut.dequeue(), input)
-    }
-    
-    func test_dequeue의_반환item의_value가_같은지_확인() {
-        let input: Item = Item(value: 13.0, operator: .addition)
-        sut.enqueue(input)
-        XCTAssertEqual(sut.dequeue()?.value, input.value)
     }
     
     func test_dequeue의_반환값이_nil인지_확인() {
@@ -41,9 +35,9 @@ final class QueueTests: XCTestCase {
     }
 
     func test_dequeue가_제대로_되었는지_확인() {
-        let input: Item = Item(value: 123, operator: .addition)
-        let input1: Item = Item(value: 456, operator: .addition)
-        let input2: Item = Item(value: 789, operator: .addition)
+        let input: Operator = .addition
+        let input1: Operator = .subtraction
+        let input2: Operator = .multiplication
         sut.enqueue(input)
         sut.enqueue(input1)
         sut.enqueue(input2)
@@ -52,7 +46,7 @@ final class QueueTests: XCTestCase {
     }
 
     func test_allClear를_확인() {
-        let input: Item = Item(value: 123, operator: .addition)
+        let input: Operator = .division
         sut.enqueue(input)
         sut.enqueue(input)
         sut.enqueue(input)
