4e537d8636ea216778d1b7c0c49b0955b1960d5d
Jason <ggj00125@gmail.com>
2023-01-18 10:22:47 +0900

    Feat: UML에 맞춰 String 타입을 확장하여 split 메서드 정의

---
+++ Calculator/Calculator/Model/Extension/String.swift
@@ -0,0 +1,14 @@
+//
+//  String.swift
+//  Calculator
+//
+//  Created by Jason on 2023/01/18.
+//
+
+import Foundation
+
+extension String {
+    func split(with target: Character) -> [String] {
+        return [String]()
+    }
+}
