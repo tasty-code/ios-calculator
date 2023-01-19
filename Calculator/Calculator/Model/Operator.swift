a953b3bd16d2e2a75bc4852e90cd5aace27875b1
Jason <ggj00125@gmail.com>
2023-01-19 10:57:13 +0900

    Feat: Operator의 multifly 메서드 테스트 성공 코드 구현
    - case의 위치와 동일하게 구문 위치 변경

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -19,10 +19,10 @@ enum Operator: Character, CalculatorItem, CaseIterable {
             return add(lhs: lhs, rhs: rhs)
         case .subtract:
             return subtract(lhs: lhs, rhs: rhs)
-        case .divide:
-            return divide(lhs: lhs, rhs: rhs)
         case .multiply:
             return multiply(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
         }
     }
     
@@ -32,10 +32,10 @@ enum Operator: Character, CalculatorItem, CaseIterable {
     private func subtract(lhs: Double, rhs: Double) -> Double {
         return lhs - rhs
     }
-    private func divide(lhs: Double, rhs: Double) -> Double {
-        return Double()
-    }
     private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+    private func divide(lhs: Double, rhs: Double) -> Double {
         return Double()
     }
 }
