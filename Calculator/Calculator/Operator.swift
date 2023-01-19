ca942a98db6ae2aa4ea727ebcff5a02bbe1a824f
devxsby <devxsby@gmail.com>
2023-01-19 15:38:43 +0900

    feat: Operator 열거형 내부 구현

--- Calculator/Calculator/Operator.swift
+++ Calculator/Calculator/Operator.swift
@@ -8,14 +8,38 @@
 import Foundation
 
 enum Operator: Character, CalculateItem, CaseIterable {
-    case add = "+"
-    case substract = "-"
-    case divide = "%"
-    case multiply = "x"
+    case add = "a"
+    case substract = "s"
+    case divide = "d"
+    case multiply = "m"
     
-    func calculate(lhs: Double, rhs: Double) -> Double {}
-    private func add(lhs: Double, rhs: Double) -> Double {}
-    private func substract(lhs: Double, rhs: Double) -> Double {}
-    private func divide(lhs: Double, rhs: Double) -> Double {}
-    private func multiply(lhs: Double, rhs: Double) -> Double {}
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .substract:
+            return substract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func substract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        // TODO:- 0으로 나눌 때 처리하기
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
 }
