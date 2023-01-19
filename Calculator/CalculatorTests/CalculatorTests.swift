98c5009097be856361fb28c6d3f449320cca8cf0
ueunli <cdenen20@gmail.com>
2023-01-19 11:24:26 +0900

    [test] 테스트코드 수정
    - given-when-then패턴에 맞도록 수정

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -22,34 +22,47 @@ final class CalculatorTests: XCTestCase {
 
     func test_빈배열이면_true이다() {
         let result = sut.isEmpty()
+        
         XCTAssertTrue(result)
     }
 
     func test_빈배열이_아니면_false이다() {
-        let result = {
-            sut.enqueue(item: "a")
-            return sut.isEmpty()
-        }()
+        let input = "a"
+        
+        sut.enqueue(item: "a")
+        let result = sut.isEmpty()
+        
         XCTAssertFalse(result)
     }
 
     func test_배열의_요소_개수를_반환한다() {
-        sut.enqueue(item: "a")
-        sut.enqueue(item: "b")
+        let inputA = "a"
+        let inputB = "b"
+        
+        sut.enqueue(item: inputA)
+        sut.enqueue(item: inputB)
         let result = sut.count()
+        
         XCTAssertEqual(result, 2)
     }
 
     func test_배열에_추가한_순서대로_저장된다() {
-        sut.enqueue(item: "a")
-        sut.enqueue(item: "b")
+        let inputA = "a"
+        let inputB = "b"
+        
+        sut.enqueue(item: inputA)
+        sut.enqueue(item: inputB)
         let result = sut.items
+        
         XCTAssertEqual(result, ["a", "b"])
     }
 
     func test_배열에_추가한_순서대로_삭제된다() {
-        sut.enqueue(item: "a")
-        sut.enqueue(item: "b")
+        let inputA = "a"
+        let inputB = "b"
+        
+        sut.enqueue(item: inputA)
+        sut.enqueue(item: inputB)
         do {
             try sut.dequeue()
             let result = sut.items
