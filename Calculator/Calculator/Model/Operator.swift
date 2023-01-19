ecc16eaabe3c57c44be08894397c7fdea39866c1
Jason <ggj00125@gmail.com>
2023-01-19 10:53:03 +0900

    Feat: Operator의 Subtract 테스트 성공 코드 구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -30,7 +30,7 @@ enum Operator: Character, CalculatorItem, CaseIterable {
         return lhs + rhs
     }
     private func subtract(lhs: Double, rhs: Double) -> Double {
-        return Double()
+        return lhs - rhs
     }
     private func divide(lhs: Double, rhs: Double) -> Double {
         return Double()
