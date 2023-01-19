62c1187c67154e22862dd8890abea7476150f060
Jason <ggj00125@gmail.com>
2023-01-19 11:19:49 +0900

    Test: Operator의 calculate 메서드 테스트 성공 코드 구현

--- Calculator/OperatorTests/OperatorTests.swift
+++ Calculator/OperatorTests/OperatorTests.swift
@@ -70,4 +70,36 @@ final class OperatorTests: XCTestCase {
         let result = Operator.divide.calculate(lhs: inputLhs, rhs: inputRhs)
         XCTAssertEqual(expectation, result)
     }
+    
+    func test_Operator의_사칙연산을_관리하는_메서드를통해_결과값이_Double타입인지() {
+        // given
+        let inputLhs = 1.0
+        let inputRhs = 2.0
+        
+        let operatorArray: [Operator] = [.add, .subtract, .multiply, .divide]
+        
+        // when
+        let expectation1 = 3.0
+        let expectation2 = -1.0
+        let expectation3 = 2.0
+        let expectation4 = 0.5
+        
+        for element in operatorArray {
+            switch element {
+            case .add:
+                let addResult = Operator.add.calculate(lhs: inputLhs, rhs: inputRhs)
+                XCTAssertEqual(expectation1, addResult)
+            case .subtract:
+                let subtractResult = Operator.subtract.calculate(lhs: inputLhs, rhs: inputRhs)
+                XCTAssertEqual(expectation2, subtractResult)
+            case .multiply:
+                let multiflyResult = Operator.multiply.calculate(lhs: inputLhs, rhs: inputRhs)
+                XCTAssertEqual(expectation3, multiflyResult)
+            case .divide:
+                let divideResult = Operator.divide.calculate(lhs: inputLhs, rhs: inputRhs)
+                XCTAssertEqual(expectation4, divideResult)
+            }
+        }
+        
+    }
 }
