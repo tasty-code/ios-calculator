0b6d40bae52a0b16224d32c48a8a8c71b306a3ba
ohdair <ohdiar@gmail.com>
2023-01-19 17:17:02 +0900

    test: input 수정

--- Calculator/QueueTests/ComponentsByOperatorsTests.swift
+++ Calculator/QueueTests/ComponentsByOperatorsTests.swift
@@ -9,14 +9,14 @@ import XCTest
 
 final class ComponentsByOperatorsTests: XCTestCase {
     func test_연산자로_split이_되는지_확인() {
-        let input = "+1+22-10*3/2+3"
+        let input = "1+22-10*3/2+3"
         let result = ExpressionParser.componentsByOperators(from: input)
         let expected = ["1","22","10","3","2","3"]
         XCTAssertEqual(result, expected)
     }
 
     func test_extractOperators의_반환값이_정확한지_확인() {
-        let input = "+1+22-10*3/2+3"
+        let input = "1+22-10*3/2+3"
         let result = ExpressionParser.extractOperators(from: input)
         let expected: [Operator] = [.add, .subtract, .multiply, .divide, .add]
         XCTAssertEqual(result, expected)
