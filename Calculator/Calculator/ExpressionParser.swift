ee9430d6627f944c46f0c08ed68c2c3e02aaa765
devxsby <devxsby@gmail.com>
2023-01-19 11:30:18 +0900

    feat: ExpressionParser 열거형 안 메서드 구현
    - parse 메서드 : separatedInputs 을 연산자와 피연산자로 분리 후 Formula 객체로 반환
    - componentsByOperators 메서드 : 문자열을 ","  기준으로 split 후 반환

--- Calculator/Calculator/ExpressionParser.swift
+++ Calculator/Calculator/ExpressionParser.swift
@@ -9,9 +9,14 @@ import Foundation
 
 enum ExpressionParser {
     func parse(from input: String) -> Formula {
-        return
+        let operators = ["a", "s", "d", "m"]
+        let separatedInputs = componentsByOperators(from: input)
+        let operatorElements = separatedInputs.filter { operators.contains($0) }
+        let operandElements = separatedInputs.filter { !operatorElements.contains($0) }
+        return Formula(operands: operandElements, operators: operatorElements)
     }
+    
     private func componentsByOperators(from input: String) -> [String] {
-        return 
+        return input.split(with: ",")
     }
 }
