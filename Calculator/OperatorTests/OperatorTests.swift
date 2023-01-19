3913ab66e19f0935d48775468dc82c2349de13bf
sloDevz <onec555@gmail.com>
2023-01-19 10:51:19 +0900

    Test: Operator의 subtract메서드 테스트 실패

--- Calculator/OperatorTests/OperatorTests.swift
+++ Calculator/OperatorTests/OperatorTests.swift
@@ -20,14 +20,29 @@ final class OperatorTests: XCTestCase {
     
     func test_Operator의_덧셈메서드를통해_결과값으로_더한값을_반환하는지() {
         // given
-        let input1 = 1.0
-        let input2 = 2.0
+        let inputLhs = 1.0
+        let inputRhs = 2.0
+        
         
         // when
         let expectation = 3.0
         
         // then
-        let result = Operator.add.calculate(lhs: input1, rhs: input2)
+        let result = Operator.add.calculate(lhs: inputLhs, rhs: inputRhs)
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_Operator의_뺄셈메서드를통해_결과값으로_뺀값을_반환하는지() {
+        // given
+        let inputLhs = 1.0
+        let inputRhs = 2.0
+        
+        
+        // when
+        let expectation = -1.0
+        
+        // then
+        let result = Operator.subtract.calculate(lhs: inputLhs, rhs: inputRhs)
         XCTAssertEqual(expectation, result)
     }
 }
