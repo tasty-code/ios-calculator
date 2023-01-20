3b3417fed57a44f7c24f19527cac952761482b02
iYeahh <120009678+iYeahh@users.noreply.github.com>
2023-01-20 11:32:35 +0900

    [feat] enum ExpressionParser에 parse(from:) 타입 메서드 구현
    - 피연산자 및 연산자들을 각각 enqueue하여 Formula 타입으로 생성 및 반환

--- Calculator/Calculator/Model/ExperssionParser.swift
+++ Calculator/Calculator/Model/ExperssionParser.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        var operators = CalculatorItemQueue<Operator>()
+        input.compactMap { Operator(rawValue: $0) }
+             .forEach { operators.enqueue(item: $0) }
+
+        var operands = CalculatorItemQueue<Double>()
+        componentsByOperators(from: input)
+            .forEach { operands.enqueue(item: Double($0) ?? 0) }
+
+        return Formula(operands: operands, operators: operators)
+    }
     private static func componentsByOperators(from input: String) -> [String] {
         input.components(separatedBy: ["+", "−", "÷", "×"])
     }
