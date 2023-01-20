11188bc0859a78a17b319cbc3f94c07d59eaf9c5
qwerty3345 <qwerty3345@naver.com>
2023-01-20 10:14:04 +0900

    test: ExpressionParser의 parse메서드가 산출한 Fomula의 result가 일치하는 값을 계산하는지 테스트

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -10,9 +10,14 @@ import XCTest
 
 final class ExpressionParserTests: XCTestCase {
 
+    let addSign = "+"
+    let subtractSign = "−"
+    let multiplySign = "×"
+    let divideSign = "÷"
+
     func test_parse메서드에_연산식을_매개변수로_넣고_실행하면_operator에는_연산자만_operands에는_피연산자만_파싱된다() {
         // given
-        let input = "1+3−20.3×99.56÷23+5.0−-9"
+        let input = "1\(addSign)3\(subtractSign)20.3\(multiplySign)99.56\(divideSign)23\(addSign)5.0\(subtractSign)-9"
         let expectationOperators: [Operator] = [.add, .subtract, .multiply, .divide, .add, .subtract]
         let expectationOperands: [Double] = [1.0, 3.0, 20.3, 99.56, 23.0, 5.0, -9.0]
 
@@ -22,5 +27,18 @@ final class ExpressionParserTests: XCTestCase {
         // then
         XCTAssertTrue(expectationOperators.allSatisfy { $0 == formula.operators.dequeue() })
         XCTAssertTrue(expectationOperands.allSatisfy { $0 == formula.operands.dequeue() })
+    }
+
+    func test_parse메서드로_산출한_Fomula의_result가_일치하는_값이다() {
+        // given
+        let input = "1\(addSign)0\(subtractSign)10\(multiplySign)5.5\(divideSign)5.5\(addSign)21\(subtractSign)1.1"
+        let expectation = 10.9
+
+        // when
+        var formula = ExpressionParser.parse(from: input)
+        let result = formula.result()
+
+        // then
+        XCTAssertEqual(result, expectation)
     }
 }
