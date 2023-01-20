6655cac45e435b6ebda6c0c508121d01895639f0
bomyuniverse <bomi@soongsil.ac.kr>
2023-01-20 10:04:02 +0900

    refactor: Operator 내의 rawValue 연산자를 UILabel과 동일하게 수정

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -12,9 +12,9 @@ final class ExpressionParserTests: XCTestCase {
 
     func test_parse메서드에_연산식을_매개변수로_넣고_실행하면_operator에는_연산자만_operands에는_피연산자만_파싱된다() {
         // given
-        let input = "1+3-20.3*99.56/23+5.0-9"
+        let input = "1+3−20.3×99.56÷23+5.0−-9"
         let expectationOperators: [Operator] = [.add, .subtract, .multiply, .divide, .add, .subtract]
-        let expectationOperands: [Double] = [1.0, 3.0, 20.3, 99.56, 23.0, 5.0, 9.0]
+        let expectationOperands: [Double] = [1.0, 3.0, 20.3, 99.56, 23.0, 5.0, -9.0]
 
         // when
         var formula = ExpressionParser.parse(from: input)
