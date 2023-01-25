d3adbf5bae12fa69725a55b4243e6db1ebbe4681
qwerty3345 <qwerty3345@naver.com>
2023-01-25 17:01:59 +0900

    feat: 숫자 operand 버튼 입력 시 검증 로직 추가

---
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -0,0 +1,78 @@
+//
+//  Calculator - CalculatorViewController.swift
+//  Created by yagom.
+//  Copyright © yagom. All rights reserved.
+//
+
+import UIKit
+
+final class CalculatorViewController: UIViewController {
+
+    // MARK: - Properties
+    @IBOutlet private weak var operandLabel: UILabel!
+    @IBOutlet private weak var operatorLabel: UILabel!
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Actions
+    @IBAction private func tappedOperandButton(_ sender: UIButton) {
+        guard var operand = operandLabel.text else { return }
+        guard let inputOperand = sender.titleLabel?.text else { return }
+
+        if operand.contains(".") && inputOperand == "." {
+            return
+        }
+
+        if (operand == "0" || operand == "-0") && (inputOperand == "0" || inputOperand == "00") {
+            return
+        }
+
+        if (operand == "0" || operand == "-0") && 1...9 ~= (Int(inputOperand) ?? 0) {
+            operand.remove(at: operand.firstIndex(of: "0") ?? operand.startIndex)
+            operandLabel.text = operand + inputOperand
+            return
+        }
+
+        operandLabel.text = operand + inputOperand
+    }
+
+    @IBAction private func tappedOperatorButton(_ sender: UIButton) {
+        operatorLabel.text = sender.titleLabel?.text
+    }
+
+    @IBAction private func tappedACButton(_ sender: UIButton) {
+        operandLabel.text = "0"
+        operatorLabel.text = String(Operator.add.rawValue)
+
+    }
+
+    @IBAction private func tappedCEButton(_ sender: UIButton) {
+        operandLabel.text = "0"
+    }
+
+    @IBAction private func tappedSignChangeButton(_ sender: UIButton) {
+        guard var operand = operandLabel.text else {
+            return
+        }
+
+//        if operand == "0" {
+//            return
+//        }
+
+        if operand.first == "-" {
+            operand.removeFirst()
+            operandLabel.text = String(operand)
+        } else {
+            operandLabel.text = "-\(operand)"
+        }
+    }
+
+    @IBAction private func tappedResultButton(_ sender: UIButton) {
+
+    }
+
+
+}
