aed9ee14e1e0c97597737fa413f506c1406dbb5e
devxsby <devxsby@gmail.com>
2023-01-25 16:02:14 +0900

    feat: 버튼 입력 인터렉션 추가 및 display 뷰 반영

--- Calculator/Calculator/Source/Screen/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Source/Screen/Controller/CalculatorViewController.swift
@@ -8,8 +8,47 @@ import UIKit
 
 final class CalculatorViewController: UIViewController {
     
+    private var userTyping: Bool = false
+    
+    @IBOutlet weak private var displayNumberLabel: UILabel!
+    
+    @IBOutlet weak private var displayOperatorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction private func digitButtonTapped(_ sender: UIButton) {
+        let currentTitle = sender.currentTitle!
+        
+        if userTyping {
+            let currentDisplayNumber = displayNumberLabel.text!
+            displayNumberLabel.text = currentDisplayNumber + currentTitle
+        } else {
+            displayNumberLabel.text = currentTitle
+        }
+        userTyping = true
+    }
+    
+    @IBAction private func operatorButtonTapped(_ sender: UIButton) {
+        displayOperatorLabel.text = sender.currentTitle!
+    }
+    
+    @IBAction func negativeButtonTapped(_ sender: UIButton) {
+        print("negativeButtonTapped")
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: UIButton) {
+        print("clearButtonTapped")
+    }
+    
+    @IBAction func allClearButtonTapped(_ sender: UIButton) {
+        print("allClearButtonTapped")
+    }
+    
+    @IBAction func equalButtonTapped(_ sender: UIButton) {
+        print("equalButtonTapped")
+    }
+    
 }
 
