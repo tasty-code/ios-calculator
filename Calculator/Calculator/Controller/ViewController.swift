153253ca4b05e28ea910b067c01568d43ac9b967
Lust3r <s1skyblue1s@gmail.com>
2023-01-25 16:55:06 +0900

    feat: isOperator 함수 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,11 +8,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var calculationFormula: String = "+"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addNumber(_ sender: UIButton) {
+        guard let number = sender.currentTitle else {
+            return
+        }
+        print(isOperator())
+    }
+    func isOperator() -> Bool {
+        var result = false
+        for `operator` in Operator.allCases {
+            guard calculationFormula.hasSuffix(String(`operator`.rawValue)) else {
+                continue
+            }
+            result = true
+        }
+        return result
+    }
 }
 
