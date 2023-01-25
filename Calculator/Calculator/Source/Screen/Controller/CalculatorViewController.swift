e5a05cce0008df1eb130bfd03b5763d1a711226b
longlivedrgn <kimye9808@naver.com>
2023-01-25 17:24:40 +0900

    feat: 연산자 버튼 클릭 시 stackView에 사용자 입력 값 반영

--- Calculator/Calculator/Source/Screen/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Source/Screen/Controller/CalculatorViewController.swift
@@ -10,8 +10,9 @@ final class CalculatorViewController: UIViewController {
     
     private var userTyping: Bool = false
     
+    @IBOutlet weak private var scrollView: UIScrollView!
+    @IBOutlet weak private var stackView: UIStackView!
     @IBOutlet weak private var displayNumberLabel: UILabel!
-    
     @IBOutlet weak private var displayOperatorLabel: UILabel!
     
     override func viewDidLoad() {
@@ -31,22 +32,33 @@ final class CalculatorViewController: UIViewController {
     }
     
     @IBAction private func operatorButtonTapped(_ sender: UIButton) {
+        let label = UILabel()
+        label.textColor = .white
+        label.text = displayOperatorLabel.text! + " " + displayNumberLabel.text!
+        
+        scrollView.layoutIfNeeded()
+        let bottomOffset = CGPoint(x: 0, y: scrollView.contentSize.height - scrollView.bounds.height)
+        scrollView.setContentOffset(bottomOffset, animated: false)
+        stackView.addArrangedSubview(label)
+        displayNumberLabel.text = ""
         displayOperatorLabel.text = sender.currentTitle!
     }
     
-    @IBAction func negativeButtonTapped(_ sender: UIButton) {
+    @IBAction private func negativeButtonTapped(_ sender: UIButton) {
         print("negativeButtonTapped")
     }
     
-    @IBAction func clearButtonTapped(_ sender: UIButton) {
+    @IBAction private func clearButtonTapped(_ sender: UIButton) {
         print("clearButtonTapped")
     }
     
-    @IBAction func allClearButtonTapped(_ sender: UIButton) {
-        print("allClearButtonTapped")
+    @IBAction private func allClearButtonTapped(_ sender: UIButton) {
+        displayNumberLabel.text = "0"
+        displayOperatorLabel.text = ""
+        stackView.subviews.forEach({ $0.removeFromSuperview() })
     }
     
-    @IBAction func equalButtonTapped(_ sender: UIButton) {
+    @IBAction private func equalButtonTapped(_ sender: UIButton) {
         print("equalButtonTapped")
     }
     
